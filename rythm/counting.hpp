830eb11e6555100e155126f6e63fe2e03ad8b6a3
Stefan Hagen <stefan@hagen.link>
2020-11-21 07:04:26 +0100

    Update counting.hpp

--- rythm/counting.hpp
+++ rythm/counting.hpp
@@ -1,3 +1,6 @@
 #pragma once
 
-constexpr auto n_times = [](size_t n) { return [n]() mutable { return n ? n-- : 0; }; };
+constexpr auto n_ramp = [](size_t n) { return [n]() mutable { return n ? n-- : 0; }; };
+
+constexpr auto n_sawtooth = [](size_t n) { 
+    auto period = n; return [period, n]() mutable { return !--( n ? n : n = period); }; }
