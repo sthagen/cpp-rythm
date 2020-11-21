0fa23cdf7a70e33747b9ef30bf0f7707d58867d5
Stefan Hagen <stefan@hagen.link>
2020-11-21 11:52:15 +0100

    Update counting.hpp

--- rythm/counting.hpp
+++ rythm/counting.hpp
@@ -2,6 +2,3 @@
 
 constexpr auto drop_n = [](auto n) { return [n]() mutable { return n ? !n-- : true; }; };
 constexpr auto take_n = [](auto n) { return [n]() mutable { return n ? !!n-- : false; }; };
-
-constexpr auto n_sawtooth = [](auto n) { 
-    auto period = n; return [period, n]() mutable { return !--( n ? n : n = period); }; };
