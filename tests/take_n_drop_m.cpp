5d1916b57e77681cb9da2cc421b603697cdf9c5e
Stefan Hagen <stefan@hagen.link>
2020-12-07 19:15:09 +0100

    Create take_n_drop_m.cpp

---
+++ tests/take_n_drop_m.cpp
@@ -0,0 +1,23 @@
+#include "catch.hpp"
+#include "rythm/counting.hpp"
+
+TEST_CASE("Take 4 and drop 1 always alternating (checking three periods a 4 + 1)", "[positive]") {
+    auto const take_4_drop_1 = take_n_drop_m(4, 1);
+    CHECK(take_4_drop_1());
+    CHECK(take_4_drop_1());
+    CHECK(take_4_drop_1());
+    CHECK(take_4_drop_1());
+    CHECK(!take_4_drop_1());
+    
+    CHECK(take_4_drop_1());
+    CHECK(take_4_drop_1());
+    CHECK(take_4_drop_1());
+    CHECK(take_4_drop_1());
+    CHECK(!take_4_drop_1());
+    
+    CHECK(take_4_drop_1());
+    CHECK(take_4_drop_1());
+    CHECK(take_4_drop_1());
+    CHECK(take_4_drop_1());
+    REQUIRE(!take_4_drop_1());    
+}
