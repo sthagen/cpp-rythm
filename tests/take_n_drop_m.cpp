f9444ca01709bfaa8124c07d77a20e3d1094dad6
Stefan Hagen <stefan@hagen.link>
2020-12-07 19:44:56 +0100

    Update take_n_drop_m.cpp

--- tests/take_n_drop_m.cpp
+++ tests/take_n_drop_m.cpp
@@ -1,5 +1,5 @@
 #include "catch.hpp"
-#include "../include/rythm/counting.hpp"
+#include "../rythm/counting.hpp"
 
 TEST_CASE("Take 4 and drop 1 always alternating (checking three periods a 4 + 1)", "[positive]") {
     auto take_4_drop_1 = take_n_drop_m(4, 1);
