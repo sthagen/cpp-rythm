28f50a19fad2fc36da7beaef4cbf8898d96f2df2
Stefan Hagen <stefan@hagen.link>
2020-12-07 20:34:05 +0100

    Update drop_n.cpp

--- tests/drop_n.cpp
+++ tests/drop_n.cpp
@@ -8,3 +8,14 @@ TEST_CASE("Dropping only first and then always taking (three times)", "[positive
     CHECK(drop_1());
     REQUIRE(drop_1());
 }
+
+TEST_CASE("Dropping first three and then always taking (four times)", "[positive]") {
+    auto drop_3 = drop_n(3);
+    CHECK(!drop_3());
+    CHECK(!drop_3());
+    CHECK(!drop_3());
+    CHECK(drop_1());
+    CHECK(drop_1());
+    CHECK(drop_1());
+    REQUIRE(drop_1());
+}
