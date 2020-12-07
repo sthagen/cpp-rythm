f0ec91c00a8a6ac3ec056a1ec5c1d658dd59a469
Stefan Hagen <stefan@hagen.link>
2020-12-07 19:24:54 +0100

    Update drop_nth.cpp

--- tests/drop_nth.cpp
+++ tests/drop_nth.cpp
@@ -1,8 +1,8 @@
 #include "catch.hpp"
-#include "rythm/counting.hpp"
+#include "../include/rythm/counting.hpp"
 
 TEST_CASE("Taking only upon every other invocation (three periods check)", "[positive]") {
-    auto const drop_every_other = drop_nth(2);
+    auto drop_every_other = drop_nth(2);
     CHECK(drop_every_other());
     CHECK(!drop_every_other());
     CHECK(drop_every_other());
