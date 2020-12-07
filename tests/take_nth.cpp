d023240f0f0f1594f0bef4b1e5bfc2a1705c0ec3
Stefan Hagen <stefan@hagen.link>
2020-12-07 23:02:34 +0100

    Update take_nth.cpp

--- tests/take_nth.cpp
+++ tests/take_nth.cpp
@@ -13,3 +13,24 @@ TEST_CASE("Taking only every third invocation (three periods check)", "[positive
     CHECK(!take_every_third());
     REQUIRE(take_every_third());
 }
+
+TEST_CASE("Taking every fifth invocation (three periods check)", "[positive]") {
+    auto take_every_fifth = take_nth(5);
+    CHECK(!take_every_third());
+    CHECK(!take_every_third());
+    CHECK(!take_every_third());
+    CHECK(!take_every_third());
+    CHECK(take_every_third());
+
+    CHECK(!take_every_third());
+    CHECK(!take_every_third());
+    CHECK(!take_every_third());
+    CHECK(!take_every_third());
+    CHECK(take_every_third());
+
+    CHECK(!take_every_third());
+    CHECK(!take_every_third());
+    CHECK(!take_every_third());
+    CHECK(!take_every_third());
+    REQUIRE(take_every_third());
+}
