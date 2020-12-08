cba04eb4340efcfe1630d195a906c421615bf196
Stefan Hagen <stefan@hagen.link>
2020-12-08 12:51:41 +0100

    Update drop_n.cpp
    maybe use fmt library ...?

--- examples/drop_n.cpp
+++ examples/drop_n.cpp
@@ -2,11 +2,13 @@
 #include "../rythm/counting.hpp"
 
 int main() {
+    auto n = 2;
+    std::cout << "Example: Using drop_n(" << n << ") to drop the first " << n << " iterations.\n";
     auto drop_2 = drop_n(2);
     auto how_many = 0;
     while (!drop_2()) {
-        std::cout << "Iteration " << ++how_many << "\n";
+        std::cout << "  " << ++how_many << ": Dropping ...\n";
     }
-    std::cout << "Finished after " << how_many << " iterations.\n";
+    std::cout << "Taking after " << how_many << " iterations.\n";
     return how_many == 2 ? 0 : 1;
 }
