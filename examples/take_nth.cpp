ba5d57d3cfba5e170338deb57d7f30f368e6d811
Stefan Hagen <stefan@hagen.link>
2020-12-08 14:38:13 +0100

    Create take_nth.cpp

---
+++ examples/take_nth.cpp
@@ -0,0 +1,18 @@
+#include <iostream>
+#include "../rythm/counting.hpp"
+
+int main() {
+    auto n = 3;
+    std::cout << "Example: Using take_nth(" << n << ") to take every " << n << ". iteration.\n";
+    auto take_every_third = take_nth(n);
+    auto how_many = 0;
+    while (!take_every_third()) {
+        std::cout << "  " << ++how_many << ": Dropping ...\n";
+    }
+    std::cout << "Taking after " << how_many << " iterations.\n";
+    while (!take_every_third()) {
+        std::cout << "  " << ++how_many << ": Dropping ...\n";
+    }
+    std::cout << "Taking after " << how_many << " iterations.\n";
+    return how_many == 2 * (n - 1) ? 0 : 1;
+}
