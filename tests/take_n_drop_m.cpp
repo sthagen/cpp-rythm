a3ff9f40312741c2f20965b828aee60f162be5ee
Stefan Hagen <stefan@hagen.link>
2020-12-08 12:36:15 +0100

    Update take_n_drop_m.cpp

--- tests/take_n_drop_m.cpp
+++ tests/take_n_drop_m.cpp
@@ -9,6 +9,14 @@ TEST_CASE("Take nothing and drop nothing always alternating (checking three time
     [[maybe_unused]] INFO("Note behaviour for take_n_drop_m(0, 0) - equivalent to take_n(0) and drop_n(1)");
 }
 
+TEST_CASE("Take nothing and drop everything always alternating (checking three times)", "[positive]") {
+    auto take_0_drop_1 = take_n_drop_m(0, 1);
+    CHECK(!take_0_drop_1());
+    CHECK(!take_0_drop_1());
+    REQUIRE(!take_0_drop_1());
+    [[maybe_unused]] INFO("Note behaviour for take_n_drop_m(0, 1) - equivalent to take_n(0) and drop_n(1)");
+}
+
 TEST_CASE("Take 1 and drop 1 always alternating (checking three periods a 1 + 1)", "[positive]") {
     auto take_1_drop_1 = take_n_drop_m(1, 1);
     CHECK(take_1_drop_1());
