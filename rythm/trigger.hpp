fd7e95cb928e855622247e854bbca0f91ff0cb29
Stefan Hagen <stefan@hagen.link>
2020-12-09 21:39:52 +0100

    Update trigger.hpp

--- rythm/trigger.hpp
+++ rythm/trigger.hpp
@@ -1,5 +1,21 @@
 #pragma once
-
+/*
+  Schmitt Trigger
+  ===============
+  
+  A Schmitt trigger implements a hysteresis by applying positive feedback to the noninverting input. 
+  The function is a "trigger" because the output retains its value until the input changes sufficiently 
+  to trigger a change. 
+  
+  > In the non-inverting configuration, when the input is higher than a chosen threshold, the output is high. 
+  > When the input is below a different (lower) chosen threshold the output is low, and when the input is 
+  > between the two levels the output retains its value. 
+  > This dual threshold action is called hysteresis and implies that the Schmitt trigger possesses memory 
+  > and can act as a bistable multivibrator (latch or flip-flop). 
+  > There is a close relation between the two kinds of circuits: 
+  >   a Schmitt trigger can be converted into a latch and a latch can be converted into a Schmitt trigger.
+                                                        (cf. https://en.wikipedia.org/wiki/Schmitt_trigger)
+*/
 namespace schmitt {
 
 template <class T>
