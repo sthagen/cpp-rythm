308c1fe7b0295fef23d9d6fca0546bee514ecd85
Stefan Hagen <stefan@hagen.link>
2020-12-09 20:18:26 +0100

    Update schmitt_trigger.cpp

--- tests/schmitt_trigger.cpp
+++ tests/schmitt_trigger.cpp
@@ -8,3 +8,26 @@ TEST_CASE("Two probes across the thresholds", "[positive]") {
     CHECK(!st(0.f));
     REQUIRE(st(high + 1.f));
 }
+
+TEST_CASE("Cycling (once)", "[positive]") {
+    auto const low = 1.f, high = 2.f;
+    bool const default_out = false;
+    auto st = schmitt_trigger<float>(low, high, default_out);
+    float m = 2., step = 1.f;
+    auto cycles = 1;
+    do {
+        INFO("Rising ...");
+        bool expected_state = false;
+        for (; m < high + step ; m += step) {
+            CHECK(st(m) == expected_state);
+            expected_state = true;
+        }
+        CHECK(st(m) == expected_state);
+        INFO("Sinking ...");
+        for (; m > low - step; m -= step) {
+            CHECK(st(m) == expected_state);
+        }
+        expected_state = false;
+        REQUIRE(st(m) == expected_state);
+    } while (--cycles);
+}
