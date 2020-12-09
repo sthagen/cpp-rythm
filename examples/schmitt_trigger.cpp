939b5a6912f982029b4d9c4fce075c8d1ad6067d
Stefan Hagen <stefan@hagen.link>
2020-12-09 20:27:13 +0100

    Create schmitt_trigger.cpp

---
+++ examples/schmitt_trigger.cpp
@@ -0,0 +1,27 @@
+#include <iostream>
+#include "../rythm/counting.hpp"
+
+int main() {
+    auto const low = -1.f, high = 2.f;
+    bool const default_out = false;
+    
+    auto st = schmitt_trigger<float>(low, high, default_out);
+   
+    std::cout << "st.context("
+              << low << ", " << high << ", " << default_out << "):\n";
+    float m = 2., step = 1.f;
+    auto cycles = 2;
+    do {
+        std::cout << "Rising ...\n";
+        for (; m < high + step ; m += step) {
+            std::cout << "  st(" << m << ") --> \t" << st(m) << "\n";
+        }
+        std::cout << "  st(" << m << ") --> \t" << st(m) << "\n";
+        std::cout << "Sinking ...\n";
+        for (; m > low - step; m -= step) {
+            std::cout << "  st(" << m << ") --> \t" << st(m) << "\n";
+        }
+        std::cout << "  st(" << m << ") --> \t" << st(m) << "\n";
+    } while (--cycles);
+    return 0;
+}
