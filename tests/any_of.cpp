a185b84c7a1bbd84ae9ba038fd380d74cd512c5b
Stefan Hagen <stefan@hagen.link>
2020-12-09 22:11:46 +0100

    Create any_of.cpp

---
+++ tests/any_of.cpp
@@ -0,0 +1,7 @@
+#include "../rythm/predicate.hpp"
+
+TEST_CASE("Any of a single true and otherwise false values yields true", "[positive]") {
+    bool const cf{false};
+    bool t{true}, f{false};
+    REQUIRE(any_of(false, cf, false, f, false, t));
+}
