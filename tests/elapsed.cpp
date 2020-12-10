98681fec702f7519f79b0d7b3d8695459b62c7fc
Stefan Hagen <stefan@hagen.link>
2020-12-10 22:48:31 +0100

    Update elapsed.cpp

--- tests/elapsed.cpp
+++ tests/elapsed.cpp
@@ -54,3 +54,9 @@ TEST_CASE("Elapsed signed integral called with start value minus wun returns min
     auto events = elapsed<int>(1);
     REQUIRE(events(0) == -1);
 }
+
+TEST_CASE("Elapsed float called with start value minus wun returns minus wun", "[positive]") {
+    using namespace Catch::literals;
+    auto events = elapsed<float>(1.f);
+    REQUIRE(events(0.f) == -1.0_a);
+}
