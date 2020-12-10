27666f8fb93ecdd51ecdf7ea127a918473945327
Stefan Hagen <stefan@hagen.link>
2020-12-10 23:06:21 +0100

    Update elapsed.cpp

--- examples/elapsed.cpp
+++ examples/elapsed.cpp
@@ -5,7 +5,7 @@ int main() {
     size_t const initial = 42;
     auto events = initial;
     std::cout << "Example: Using elapsed(" << initial << ") to create some counter and evaluating after " << events << " additional events.\n";
-    auto count = elapsed<size_t>(event_counts);
+    auto count = elapsed<size_t>(events);
     events += events;
     auto duration = count(events);
     std::cout << "Stopping after " << duration << " events.\n";
