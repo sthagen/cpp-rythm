ff0258d811c479a51b8058a449762f36757434f1
Stefan Hagen <stefan@hagen.link>
2020-12-10 22:46:03 +0100

    Update elapsed.cpp

--- tests/elapsed.cpp
+++ tests/elapsed.cpp
@@ -23,29 +23,29 @@ TEST_CASE("Elapsed double called with start value returns zero", "[positive]") {
     REQUIRE(events(0.) == 0.0_a);
 }
 
-TEST_CASE("Elapsed unsigned integral called with start value plus wun returns returns wun", "[positive]") {
+TEST_CASE("Elapsed unsigned integral called with start value plus wun returns wun", "[positive]") {
     auto events = elapsed<size_t>(0);
     REQUIRE(events(1) == 1);
 }
 
-TEST_CASE("Elapsed signed integral called with start value plus wun returns returns wun", "[positive]") {
+TEST_CASE("Elapsed signed integral called with start value plus wun returns wun", "[positive]") {
     auto events = elapsed<int>(0);
     REQUIRE(events(1) == 1);
 }
 
-TEST_CASE("Elapsed float called with start value returns plus wun returns returns wun", "[positive]") {
+TEST_CASE("Elapsed float called with start value returns plus wun returns wun", "[positive]") {
     using namespace Catch::literals;
     auto events = elapsed<float>(0.f);
     REQUIRE(events(1.f) == 1.0_a);
 }
 
-TEST_CASE("Elapsed double called with start value returns plus wun returns returns wun", "[positive]") {
+TEST_CASE("Elapsed double called with start value returns plus wun returns wun", "[positive]") {
     using namespace Catch::literals;
     auto events = elapsed<double>(0.);
     REQUIRE(events(1.) == 1.0_a);
 }
 
-TEST_CASE("Elapsed unsigned integral called with start value minus wun returns returns maximal value (size_t)", "[positive]") {
+TEST_CASE("Elapsed unsigned integral called with start value minus wun returns maximal value (size_t)", "[positive]") {
     auto events = elapsed<size_t>(1);
     REQUIRE(events(0) == 0xffff'ffff'ffff'ffff);
 }
