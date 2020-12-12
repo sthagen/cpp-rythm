c0cb9de4b4fd17be2e98c9f21b2be22b5f94c8cd
Stefan Hagen <stefan@hagen.link>
2020-12-12 22:05:21 +0100

    Update count_else.cpp

--- tests/count_else.cpp
+++ tests/count_else.cpp
@@ -6,3 +6,10 @@ TEST_CASE("Count else for predicate matching does not increment", "[positive]") 
     auto small_cases = count_else<decltype(more_than_five), size_t>(more_than_five);
     REQUIRE(small_cases(42) == 0);
 }
+
+TEST_CASE("Count else for predicate not matching does increment", "[positive]") {
+    auto const threshold = 5;
+    auto more_than_five = [](auto x){ return x > threshold;};
+    auto small_cases = count_else<decltype(more_than_five), size_t>(more_than_five);
+    REQUIRE(small_cases(threshold - 1) == 1);
+}
