83dca66b2a0ac9309034f9acfebe8ac1353a36e4
Stefan Hagen <stefan@hagen.link>
2020-12-12 18:40:43 +0100

    Fixed copy-pasta

--- tests/all_of.cpp
+++ tests/all_of.cpp
@@ -63,5 +63,5 @@ TEST_CASE("Zero if all of only a single true value zeroes the target", "[positiv
 TEST_CASE("Other if all of only a single true value sets the target to the other value", "[positive]") {
     bool t{true};
     double k{42.}, o{3.14156};
-    REQUIRE(zero_if_all_of(k, o, t) == o);
+    REQUIRE(other_if_all_of(k, o, t) == o);
 }
