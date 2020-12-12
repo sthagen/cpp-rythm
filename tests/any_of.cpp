aabbec81e05626242d1b91ea833113edfad8a07d
Stefan Hagen <stefan@hagen.link>
2020-12-12 17:48:30 +0100

    Update any_of.cpp

--- tests/any_of.cpp
+++ tests/any_of.cpp
@@ -12,3 +12,9 @@ TEST_CASE("Any of false values yields false", "[positive]") {
     bool f{false};
     REQUIRE(!any_of(false, cf, false, f, false));
 }
+
+TEST_CASE("Any of not only true values yields true", "[positive]") {
+    bool const ct{true};
+    bool t{true}, f{false};
+    REQUIRE(any_of(true, ct, true, t, true, f));
+}
