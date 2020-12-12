4cf1d492d9876558c09ea596417cec3fc40f7d0f
Stefan Hagen <stefan@hagen.link>
2020-12-12 18:00:30 +0100

    Create any_of.cpp

---
+++ examples/any_of.cpp
@@ -0,0 +1,11 @@
+#include <iostream>
+#include "../rythm/predicate.hpp"
+
+int main() {
+    size_t const answer = 42;
+    auto oh_no = 0;
+    std::cout << "Example: Using any_pf(" << answer << ", " << oh_no << ".\n";
+    auto outcome = any_of(answer, oh_no);
+    std::cout << "Outcome is " << outcome << ".\n";
+    return outcome ? 0 : 1;
+}
