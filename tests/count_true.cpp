6ffcb9d0f69edb5e7e905dd729ee3b0d5f7e21e0
Stefan Hagen <stefan@hagen.link>
2020-12-12 22:00:49 +0100

    Update count_true.cpp

--- tests/count_true.cpp
+++ tests/count_true.cpp
@@ -2,6 +2,6 @@
 #include "../rythm/counting.hpp"
 
 TEST_CASE("Count true for true increments", "[positive]") {
-    auto successes = count_true();
+    auto successes = count_true<size_t>();
     REQUIRE(successes(true) == 1);
 }
