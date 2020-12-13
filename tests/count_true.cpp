1f3acc3f7f30ab755d295e72c599eb970230b655
Stefan Hagen <stefan@hagen.link>
2020-12-13 15:00:53 +0100

    Update count_true.cpp

--- tests/count_true.cpp
+++ tests/count_true.cpp
@@ -7,6 +7,12 @@ TEST_CASE("Count true for true does increment", "[positive]") {
 }
 
 TEST_CASE("Count true for false does not increment", "[positive]") {
-    auto failusuccessesres = count_false<size_t>();
+    auto successes = count_true<size_t>();
     REQUIRE(successes(false) == 0);
+}
+
+TEST_CASE("Count true for true and then false does increment once", "[positive]") {
+    auto successes = count_true<size_t>();
+    CHECK(successes(true) == 1);
+    REQUIRE(successes(false) == 1);
 }
