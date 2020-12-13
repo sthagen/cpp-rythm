f3f95dffb3a0a23a9ad246e094fa562290d7b449
Stefan Hagen <stefan@hagen.link>
2020-12-13 15:07:12 +0100

    Update count_zero.cpp

--- tests/count_zero.cpp
+++ tests/count_zero.cpp
@@ -7,6 +7,12 @@ TEST_CASE("Count zero for non-zero does not increment", "[positive]") {
 }
 
 TEST_CASE("Count zero for zero does increment", "[positive]") {
-    auto zeroes = count_zero<size_t>();
+    auto zeroes = count_zero<size_t, unsigned short>();
     REQUIRE(zeroes(0) == 1);
+}
+
+TEST_CASE("Count zero for zero and then non-zero does increment once", "[positive]") {
+    auto zeroes = count_zero<size_t, unsigned short>();
+    CHECK(zeroes(0) == 0);
+    REQUIRE(zeroes(42) == 1);
 }
