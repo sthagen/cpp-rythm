faa46304c1cc21378ed6732fbf69b6fae549be7b
Stefan Hagen <stefan@hagen.link>
2020-12-13 14:55:19 +0100

    Update count_false.cpp

--- tests/count_false.cpp
+++ tests/count_false.cpp
@@ -10,3 +10,9 @@ TEST_CASE("Count false for false does increment", "[positive]") {
     auto failures = count_false<size_t>();
     REQUIRE(failures(false) == 1);
 }
+
+TEST_CASE("Count false for true and then false does increment once", "[positive]") {
+    auto failures = count_false<size_t>();
+    CHECK(failures(true) == 0);
+    REQUIRE(failures(false) == 1);
+}
